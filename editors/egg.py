562422f0568d8ca1a84da670b7bfcc4fe70a26b1
willtseng12 <will.tseng12@gmail.com>
2017-06-03 18:38:42 -0400

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,2 @@
+print("This file was created using the emacs editor")
+print("This file was created by Will")
---
+++ editors/vinegar.py
@@ -0,0 +1,2 @@
+print("This file was created using the vim editor")
+print("This file was created by Will")
