80aaf9240ccb530554ddc103ddf9b4f67a2f6044
Will <will@Willde-MacBook-Pro.local>
2017-06-06 17:00:13 -0400

    answered advance python part 2

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+
+import csv
+
+# reading in file
+
+lol = []
+with open('faculty.csv', 'r') as csvfile:
+    readCSV = csv.reader(csvfile)
+    for faculty in readCSV:
+        lol.append(faculty)
+        
+        
+emails = []
+for faculty in lol[1:]:
+    emails.append(faculty[3].strip())
+    
+# writing in file
+
+with open('email.csv', 'w') as file:
+    for email in emails:
+        file.write(email+'\n')
